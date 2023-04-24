c169e8ffc1346f881bb10e5454ee1342b296ce57
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-04-24 17:29:15 -0600

    feat: test 1 mutex

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@
 using namespace std;
 
 const int COORDS = 3000;
+const int THREADS = 3;
+int id[THREADS];
+int dotsInsideCircle = 0;
+pthread_t tids[THREADS];
+pthread_mutex_t mutex_lock;
+
 
 void printMatrix(double** matrix, int rows) {
     cout << "Printing matrix..." << endl;
@@ -29,28 +35,43 @@ void isDotInsideCircle(int* amount, double* dot) {
 };
 
 
-void generateCoords(int rows, int* dotsInsideCircle) {
+void* generateCoords(int d) {
+    int rows = COORDS;
     int cols = 2;
 
     srand( (unsigned)time( NULL ) );
+    pthread_mutex_lock(&mutex_lock);
     double** matrix = new double*[rows];
     for (int i = 0; i < rows; i++) {
         matrix[i] = new double[cols];
         for (int j = 0; j < cols; j++) {
             bool isNegative = (float) rand()/RAND_MAX < 0.5; 
             matrix[i][j] = (float) rand()/RAND_MAX * (isNegative ? -1 : 1);
-        }
-        isDotInsideCircle(dotsInsideCircle, matrix[i]);
-    }
-    // printMatrix(matrix, rows);  
+        };
+        isDotInsideCircle(&dotsInsideCircle, matrix[i]);
+    };
+
+    // printMatrix(matrix, rows);
+    pthread_mutex_unlock(&mutex_lock);
+    pthread_exit(NULL);
 };
 
 
-int main() {
-    // Create matrix
-    int dotsInsideCircle = 0;
+int main(int argc, char* argv[]) {
+    // for (int i = 0; i < THREADS; i++) {
+    //     state[i] = GENERATING;
+    //     id[i] = i;
+    //     pthread_cond_init(&chopsticks[i], NULL);
+    // }
+    pthread_mutex_init(&mutex_lock, NULL);
 
-    generateCoords(COORDS, &dotsInsideCircle);
+    for (int i = 0; i < THREADS; i++) {
+        pthread_create(&tids[i], NULL, generateCoords, (void*) 3);
+    }
+
+    for (int i = 0; i < THREADS; i++) {
+        pthread_join(tids[i], NULL);
+    }
 
     cout << "Amount: " << dotsInsideCircle << "/" << COORDS << endl;
 
