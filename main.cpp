7e79d1c16b0d2890e76592f7ae8faaca01c02af9
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-04-24 17:09:23 -0600

    split code

--- main.cpp
+++ main.cpp
@@ -11,16 +11,13 @@ using namespace std;
 
 const int COORDS = 3000;
 
-void generateCoords() {
-
-};
-
 void printMatrix(double** matrix, int rows) {
     cout << "Printing matrix..." << endl;
     for (int i = 0; i < rows; i++) {
         cout << "(" << fixed << setprecision(5) << matrix[i][0] << ", " << fixed << setprecision(5) << matrix[i][1] << ")" << endl;
     };
 };
+
 
 void isDotInsideCircle(int* amount, double* dot) {
     double distance = pow(pow(dot[0], 2) + pow(dot[1], 2), .5);
@@ -32,11 +29,8 @@ void isDotInsideCircle(int* amount, double* dot) {
 };
 
 
-
-int main() {
-    // Create matrix
-    int rows = COORDS, cols = 2;
-    int dotsInsideCircle = 0;
+void generateCoords(int rows, int* dotsInsideCircle) {
+    int cols = 2;
 
     srand( (unsigned)time( NULL ) );
     double** matrix = new double*[rows];
@@ -46,14 +40,23 @@ int main() {
             bool isNegative = (float) rand()/RAND_MAX < 0.5; 
             matrix[i][j] = (float) rand()/RAND_MAX * (isNegative ? -1 : 1);
         }
-        isDotInsideCircle(&dotsInsideCircle, matrix[i]);
+        isDotInsideCircle(dotsInsideCircle, matrix[i]);
     }
-    printMatrix(matrix, rows);
-    cout << "Amount: " << dotsInsideCircle << endl;
+    // printMatrix(matrix, rows);  
+};
 
-    double pi = 4.0 * dotsInsideCircle / rows;
 
-    cout <<"pi is equal to: " << fixed << setprecision(5) << pi << endl;
+int main() {
+    // Create matrix
+    int dotsInsideCircle = 0;
+
+    generateCoords(COORDS, &dotsInsideCircle);
+
+    cout << "Amount: " << dotsInsideCircle << "/" << COORDS << endl;
+
+    double pi = 4.0 * dotsInsideCircle / COORDS;
+
+    cout << "Pi is equal to: " << fixed << setprecision(5) << pi << endl;
 
     return 0;
 };
