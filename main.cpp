4c3cfc97e8c4df77910c93f5a658235feaf8d480
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-04-24 17:00:26 -0600

    feat: added distance formula

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-const int COORDS = 10;
+const int COORDS = 3;
 
 void generateCoords() {
 
@@ -22,8 +22,20 @@ void printMatrix(double** matrix, int rows) {
     };
 };
 
+void isDotInsideCircle(int* amount, double* dot) {
+    double distance = pow(pow(dot[0], 2) + pow(dot[1], 2), .5);
+
+    if (distance > 1) return;
+
+    // Its inside
+    *amount += 1;
+};
+
 int main() {
+    // Create matrix
     int rows = COORDS, cols = 2;
+    int dotsInsideCircle = 0;
+
     srand( (unsigned)time( NULL ) );
     double** matrix = new double*[rows];
     for (int i = 0; i < rows; i++) {
@@ -32,8 +44,10 @@ int main() {
             bool isNegative = (float) rand()/RAND_MAX < 0.5; 
             matrix[i][j] = (float) rand()/RAND_MAX * (isNegative ? -1 : 1);
         }
+        isDotInsideCircle(&dotsInsideCircle, matrix[i]);
     }
     printMatrix(matrix, rows);
+    cout << "Amount: " << dotsInsideCircle << endl;
 
     return 0;
 };
